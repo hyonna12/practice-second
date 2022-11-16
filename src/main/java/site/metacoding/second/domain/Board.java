c6390fe62498b21a62169b0ec0c69e47d72aa610
hyonna12 <hyonna12@gmail.com>
2022-11-16 21:58:46 +0900

    hibernate - board 테이블 자동생성

---
+++ src/main/java/site/metacoding/second/domain/Board.java
@@ -0,0 +1,22 @@
+package site.metacoding.second.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+public class Board {
+  @Id // primary key
+  @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+  private Long id;
+  private String title;
+  @Column(length = 1000)
+  private String content;
+}
